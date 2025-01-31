0c5c9eace722f29f877a8dbc96a0f2eb2209db7c
Aji Kamaludin <aji19kamaludin@gmail.com>
2025-01-31 10:56:41 +0700

    bitfix

--- app/Http/Controllers/Default/GeneralController.php
+++ app/Http/Controllers/Default/GeneralController.php
@@ -60,7 +60,7 @@ class GeneralController extends Controller
             'total_sale_month' => $total_sale_month,
             'total_sale_today' => $total_sale_today,
             'items_sale_today' => $items_sale_today,
-            'total_margin_month' => $total_purchase_month - $total_sale_month,
+            'total_margin_month' => $total_sale_month - $total_purchase_month,
             'target_percent_month' => Setting::getByKey('monthly_sales_target') > 0 ? Number::percentage(($total_sale_month / Setting::getByKey('monthly_sales_target')) * 100, 2) : '0%',
             'top_products' => $top_products,
             'charts' => $this->charts($request),
